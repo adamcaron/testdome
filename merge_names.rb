910d48c53c39f35dd5ffb6ff551e84c3da1d6ba6
Adam Caron <adamwcaron@gmail.com>
2020-04-24 15:32:44 -0400

    instrutions

--- merge_names.rb
+++ merge_names.rb
@@ -1,3 +1,7 @@
+# Implement the unique_names method. When passed two arrays of names, it will return an array containing the names that appear in either or both arrays. The returned array should have no duplicates.
+
+# For example, calling unique_names(["Ava", "Emma", "Olivia"], ["Olivia", "Sophia", "Emma"]) should return an array containing Ava, Emma, Olivia, and Sophia in any order.
+
 def unique_names(names1, names2)
   return names1.concat(names2).uniq
 end
--- quadratic_equasion.rb
+++ quadratic_equasion.rb
@@ -1,3 +1,9 @@
+# Implement the function find_roots to find the roots of the quadratic equation: ax2 + bx + c = 0. The function should return an array containing both roots in any order. If the equation has only one solution, the function should return that solution as both elements of the array. The equation will always have at least one solution.
+
+# The roots of the quadratic equation can be found with the following formula: A quadratic equation.
+
+# For example, find_roots(2, 10, 8) should return [-1, -4] or [-4, -1] as the roots of the equation 2x2 + 10x + 8 = 0 are -1 and -4.
+
 # param [Integer] a 
 # param [Integer] b 
 # param [Integer] c 
