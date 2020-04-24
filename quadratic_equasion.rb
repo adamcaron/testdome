0d7cc71ef83c73902b4ba3f28838b19ca298c7cb
Adam Caron <adamwcaron@gmail.com>
2020-04-24 15:24:47 -0400

    initial qe

---
+++ quadratic_equasion.rb
@@ -0,0 +1,11 @@
+# param [Integer] a 
+# param [Integer] b 
+# param [Integer] c 
+#
+# @return [Array[Float]] 
+#   An array of two elements containing both roots in any order
+def find_roots(a, b, c)
+  return nil
+end
+
+puts find_roots(2, 10, 8)
