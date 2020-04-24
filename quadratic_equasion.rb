f915f1650deac57fbdcecb7d39ee0fb8d3dfaf9c
Adam Caron <adamwcaron@gmail.com>
2020-04-24 15:26:15 -0400

    two real roots given at least 1 root exists

--- quadratic_equasion.rb
+++ quadratic_equasion.rb
@@ -5,7 +5,12 @@
 # @return [Array[Float]] 
 #   An array of two elements containing both roots in any order
 def find_roots(a, b, c)
-  return nil
+  radicand = (b*b) - (4*a*c)
+  numerator1 = (-1*b) + Math.sqrt(radicand)
+  numerator2 = (-1*b) - Math.sqrt(radicand)
+  root1 = numerator1 / (2*a)
+  root2 = numerator2 / (2*a)
+  return [root1, root2]
 end
 
 puts find_roots(2, 10, 8)
