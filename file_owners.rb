6ebe841b14307e306c14408868a29f75107a770b
Adam Caron <adamwcaron@gmail.com>
2020-04-24 16:45:26 -0400

    group em

--- file_owners.rb
+++ file_owners.rb
@@ -5,7 +5,12 @@
 # For example, for hash {'Input.txt' => 'Randy', 'Code.py' => 'Stan', 'Output.txt' => 'Randy'} the group_by_owners function should return {'Randy' => ['Input.txt', 'Output.txt'], 'Stan' => ['Code.py']}.
 
 def group_by_owners(files)
-  return nil
+  return files.reduce({}) do |memo, record|
+    owner = record[1]
+    file = record[0]
+    memo.has_key?(owner) ? memo[owner] << file : memo[owner] = [file]
+    memo
+  end
 end
 
 files = {
