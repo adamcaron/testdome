2ef6c49a4eef96c51759ad4eb4f4fa0402ca2303
Adam Caron <adamwcaron@gmail.com>
2020-04-30 11:42:12 -0400

    set focus on mount or update. good case for useEffect

--- react/focus.js
+++ react/focus.js
@@ -1,3 +1,9 @@
+// The TextInput component renders an input element in the DOM and accepts a ref that is forwarded to that input element. Finish the FocusableInput component:
+
+// The component should accept a focused prop.
+// When the focused prop is changed from false to true, and the input is not focused, it should receive the focus.
+// If on mounting the focused prop is true, the input should receive the focus.
+
 class Input extends React.PureComponent {
   render() {
     let {forwardedRef, ...otherProps} = this.props;
@@ -21,9 +27,17 @@ class FocusableInput extends React.Component {
   // and the input is not focused, it should receive focus.
   // If focused prop is true, the input should receive the focus.
   // Implement your solution below:
-  componentDidUpdate(prevProps) {}
+  componentDidUpdate(prevProps) {
+    if ((prevProps.focused !== this.props.focused) && this.props.focused) {
+      this.ref.current.focus()
+    }
+  }
 
-  componentDidMount() {}
+  componentDidMount() {
+    if (this.props.focused) {
+      this.ref.current.focus()
+    }
+  }
 }
 
 FocusableInput.defaultProps = {
